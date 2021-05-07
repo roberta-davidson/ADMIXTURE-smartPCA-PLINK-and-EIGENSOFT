9ff9c310f371f75745f9cfe50f7769852a543aa6
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2021-05-07 16:16:14 +0930

    Update PCA_plot.R

--- PCA_plot.R
+++ PCA_plot.R
@@ -1,24 +1,20 @@
 #read in data
 fn = "<path>/<name>.pca.evec.txt"
 evecDat = read.table(fn, col.names=c("Sample", "PC1", "PC2", "PC3", "PC4", "PC5", "PC6", 
-                                     "PC7", "PC8", "PC9", "PC10", "Pop"))
+			"PC7", "PC8", "PC9", "PC10", "Population"))
 
 #read in population groups to table for labelling purposes
-popGroups=read.table("/Users/robbi/Documents/DataAnalysis/Inca_PCA/Inca_PCA1/IncaMod+HO_NoAdm+TIW.ind", 
-                     col.names=c("Sample", "Sex", "PopGroup.a"))
+populations = read.table("<path>/<name>.popLabels.txt",col.names=c("Sample", "Label"))
+ind = read.table("<path>/<name>.ind", 
+                col.names=c("Sample", "Sex", "Population"))
 
-#file for population grouping in legend
-populations = read.table("/Users/robbi/Documents/DataAnalysis/Inca_PCA/Inca_PCA1/IncaMod+HO_NoAdm+TIW.popGroups.txt", 
-                         col.names=c("PopGroup.b", "Population"))
-mergedPopDat = cbind(popGroups, populations)
-
-#add population column 
-mergedEvecDat = merge(mergedPopDat, evecDat, by="Sample")
+#bind population colums (horizontally) for labelling first
+mergedPopDat = cbind(ind, populations)
+#merge with evec data
+mergedEvecDat3 = merge(mergedPopDat3, evecDat3, by="Sample")
 
 #plot
-plot(mergedEvecDat2$PC1, mergedEvecDat2$PC2, col=mergedEvecDat2$Population, xlab="PC1", ylab="PC2", pch=1)
-legend("left", legend=levels(mergedEvecDat2$Population), col=1:length(levels(mergedEvecDat2$Population)), pch=20)
-
-
-#to zoom to a part of the plot add this into the plot line
-#xlim=c(-0.01,0.01), ylim=c(-0.03,-0.01)
+plot(mergedEvecDat$PC1, mergedEvecDat$PC2, col=mergedEvecDat$Label, 
+			pch=as.integer(mergedEvecDat$Label) %% 24, xlab="PC1", ylab="PC2")
+legend("topright", xpd=TRUE, legend=levels(mergedEvecDat$Label), 
+			col=1:length(levels(mergedEvecDat$Label)), pch=1:length(levels(mergedEvecDat$Label)))
