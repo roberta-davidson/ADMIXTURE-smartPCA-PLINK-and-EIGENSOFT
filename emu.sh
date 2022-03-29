6d0f2af40b5313cd1d241820c2a2691a0c70485c
roberta-davidson <78726635+roberta-davidson@users.noreply.github.com>
2022-03-29 14:17:09 +1030

    Create emu.sh

---
+++ emu.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#requirements Python=2.7
+
+conda activate emu
+
+DATA=$1
+
+emu --plink ${DATA} \
+	--n_eig 10 \
+	--threads 8 \
+	--maf 0.01 \
+	--out ${DATA} \
+	--maf_save \
+	--sites_save
